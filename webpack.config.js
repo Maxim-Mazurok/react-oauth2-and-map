9ae080156485e59d5ca7aa79a8ff46417afb22c1
Maxim Mazurok <maxim@mazurok.com>
2019-08-08 19:09:31 +0300

    webpack tslint plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,67 @@
-const path = require("path");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const { CheckerPlugin } = require("awesome-typescript-loader");
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const TSLintPlugin = require('tslint-webpack-plugin');
+const { CheckerPlugin } = require('awesome-typescript-loader');
 
 module.exports = {
-  devtool: "source-map",
-  context: path.join(__dirname, "src"),
-  entry: "./index.tsx",
+  devtool: 'source-map',
+  context: path.join(__dirname, 'src'),
+  entry: './index.tsx',
   resolve: {
-    extensions: [".ts", ".tsx", ".js"],
+    extensions: ['.ts', '.tsx', '.js'],
   },
   output: {
-    path: path.join(__dirname, "dist"),
-    filename: "bundle.min.js",
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.min.js',
   },
   module: {
     rules: [
       {
         test: /\.ts(x?)$/,
         exclude: /node_modules/,
-        use: [
-          "babel-loader",
-          "awesome-typescript-loader",
-        ],
+        use: ['babel-loader', 'awesome-typescript-loader'],
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: "html-loader",
+            loader: 'html-loader',
           },
         ],
       },
       {
         test: /\.scss$/,
         loaders: [
-          "style-loader",
+          'style-loader',
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
               importLoaders: 1,
             },
           },
-          "sass-loader",
+          'sass-loader',
         ],
       },
       {
         test: /\.ico$/,
-        loader: "file-loader?name=[name].[ext]",
+        loader: 'file-loader?name=[name].[ext]',
       },
     ],
   },
   plugins: [
     new HtmlWebPackPlugin({
-      favicon: "./favicon.ico",
-      template: "./index.html",
-      filename: "./index.html",
+      favicon: './favicon.ico',
+      template: './index.html',
+      filename: './index.html',
     }),
     new CheckerPlugin(),
+    new TSLintPlugin({
+      files: [
+        './src/**/*.tsx',
+        './src/**/*.ts',
+        './test/**/*.tsx',
+        './test/**/*.ts',
+      ],
+    }),
   ],
 };
